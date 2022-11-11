33ccd26e75f2539b3e22fd7a6d7e4abd59de0e3e
Ross Jacobson <74107876+Ross-J@users.noreply.github.com>
2022-11-11 11:58:16 -0600

    Homework14_completed

--- Main.rkt
+++ Main.rkt
@@ -13,15 +13,17 @@
 
 ;(define sample-code '(call (function (a) a) (5)))
 ;(define sample-code '(call (function(x) (math + x a)) (5)))
-(define sample-code '(call (function (a) (call (function (r) a) (a))) (5)))
+;(define sample-code '(call (function (a) (call (function (r) a) (a))) (5)))
 
 
 ;(define sample-code '(local-vars ((p c)) (math / a p)))
 ;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
-;(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
+
+
+(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
 
 
 
 (displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
-(run-neo-parsed-code parsed-neo-code env)
+(displayln (run-neo-parsed-code parsed-neo-code env))
